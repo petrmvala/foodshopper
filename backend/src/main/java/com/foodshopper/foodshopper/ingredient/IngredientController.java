80556299b9a983211637419ea528cd880e8e2b38
kowodo <stepan.krivanec@gmail.com>
2021-03-21 23:30:34 +0100

    changed ingredient API

--- backend/src/main/java/com/foodshopper/foodshopper/ingredient/IngredientController.java
+++ backend/src/main/java/com/foodshopper/foodshopper/ingredient/IngredientController.java
@@ -1,45 +1,48 @@
 package com.foodshopper.foodshopper.ingredient;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/ingredient")
+@RequestMapping(path = "/v1/ingredients")
 @CrossOrigin(origins = "http://localhost:4200")
 public class IngredientController {
-    private IngredientService ingredientService;
+    private final IngredientService ingredientService;
 
     @Autowired
     public IngredientController(IngredientService ingredientService) {
         this.ingredientService = ingredientService;
     }
 
-
-    @GetMapping("all")
+    @GetMapping
     public List<Ingredient> getAllIngredients() {
         return ingredientService.getAll();
     }
 
-    @GetMapping(path = "/one/{id}")
-    public Ingredient getIngregientById(@PathVariable("id") Long id) {
+    @GetMapping(path = "/{id}")
+    public Ingredient getIngredientById(@PathVariable("id") Long id) {
         return ingredientService.getById(id);
     }
 
-    @PostMapping("post")
-    public void createIngredient(@RequestBody Ingredient ingredient) {
-        ingredientService.saveOne(ingredient);
+    @PostMapping
+    public ResponseEntity<String> createIngredient(@RequestBody IngredientDTO ingredientDTO) {
+        Ingredient ing = ingredientService.save(ingredientDTO.createIngredient());
+        return ResponseEntity.created(URI.create("/ingredients/" + ing.getId())).build();
     }
 
-    @PutMapping("/put")
-    public void updateIngredient(@RequestBody Ingredient ingredient ) {
-        ingredientService.saveOne(ingredient);
+    @PutMapping(path = "/{id}")
+    public void updateIngredient(@RequestBody IngredientDTO ingredientDTO) {
+        // send error when not updated?
+        ingredientService.save(ingredientDTO.createIngredient());
     }
 
-    @DeleteMapping("/delete/{id}")
-    public void deleteIngredientById(@PathVariable("id") Long id){
-       ingredientService.deleteOne(id);
+    @DeleteMapping("/{id}")
+    public void deleteIngredientById(@PathVariable("id") Long id) {
+        ingredientService.delete(id);
     }
 
 }
