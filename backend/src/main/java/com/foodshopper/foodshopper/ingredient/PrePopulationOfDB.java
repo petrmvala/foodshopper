154c931ebd037dac7a4f10b1d35c45b39d008281
kowodo <stepan.krivanec@gmail.com>
2021-03-27 23:38:49 +0100

    populating the database with nutridatabaze

--- backend/src/main/java/com/foodshopper/foodshopper/ingredient/PrePopulationOfDB.java
+++ backend/src/main/java/com/foodshopper/foodshopper/ingredient/PrePopulationOfDB.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.Map;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 @Component
 public class PrePopulationOfDB implements CommandLineRunner {
@@ -13,13 +15,26 @@ public class PrePopulationOfDB implements CommandLineRunner {
     private IngredientRepository ingredientRepository;
 
     @Override
-    public void run(String... args) {
+    public void run(String... args) throws IOException {
+        File nutriDatabaze = new File("NutriDatabaze-v7.16-data-export.csv");
+        try (BufferedReader reader = new BufferedReader(new FileReader(nutriDatabaze))) {
+            String line;
+            String[] splitLine;
+            String[] columnNames;
+            columnNames = reader.readLine().split(";");
+            Ingredient ingredient;
 
-        ingredientRepository.saveAll(List.of(
-                new Ingredient("Chicken", Map.of("Protein per 100g", "A fooking lot")),
-                new Ingredient("not chicken", Map.of("Protein per 100g", "not a lot")),
-                new Ingredient("orange", Map.of("Vitamin C", "fook ton")),
-                new Ingredient("radish", Map.of("Red color per piece", "all over it")),
-                new Ingredient("spinach", Map.of("Iron per 100g", "10x less than claimed by US WW2 propaganda"))));
+            while (null != (line = reader.readLine())) {
+                splitLine = line.split(";");
+//  0           1           2           3       4       5       6       7           8               9           10         11            12         13          14          15          16          17          18          19          20          21          22
+// OrigFdCd	 OrigFdNm	 EngFdNam	 SciNam	 EDIBLE	 NCF	 FACF	 ENERC [kJ]	 ENERC [kcal]	 FAT [g]	 FASAT [g]	 FAMS [g]	 FAPU [g]	 FATRN [g]	 CHOT [g]	 CHO [g]	 SUGAR [g]	 FIBT [g]	 PROT [g]	 ASH [g]	 NA [mg]	 NACL [g]	 WATER [g]
+                ingredient = new Ingredient(Long.parseLong(splitLine[0]), splitLine[2]);
+                for (int i = 1; i < columnNames.length; i++) {
+                    ingredient.addData(columnNames[i], splitLine[i]);
+                }
+                ingredientRepository.save(ingredient);
+            }
+        }
+
     }
 }
