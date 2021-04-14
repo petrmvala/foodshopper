8fae9a64ed9da2a04742614739e9b027effd88fe
kowodo <stepan.krivanec@gmail.com>
2021-04-14 23:40:38 +0200

    refactoring getPage method

--- frontend/src/app/ingredient.service.ts
+++ frontend/src/app/ingredient.service.ts
@@ -1,9 +1,10 @@
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {Ingredient} from '../types/Ingredient';
 import {Page} from '../types/Page';
 import {map} from 'rxjs/operators';
+import {Params} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -19,13 +20,22 @@ export class IngredientService {
   }
 
   getPage(page: number, size: number): Observable<Page<Ingredient>> {
-    return this.httpClient.get<Page<Ingredient>>(this.baseUrl + '/page' + '?page=' + page + '&size=' + size)
+    const params: Params = new HttpParams()
+      .set('page', String(page))
+      .set('size', String(size));
+
+    return this.httpClient
+      .get<Page<Ingredient>>(this.baseUrl + '/page', {params})
       .pipe(map(ingredientPage => {
-          ingredientPage.content.forEach(ingredient => {
-            ingredient.data = new Map<string, string>(Object.entries(ingredient.data));
-          });
-          return ingredientPage;
+        this.fillPageIngredientsMaps(ingredientPage);
+        return ingredientPage;
         })
       );
+  }
+
+  private fillPageIngredientsMaps(ingredientPage: Page<Ingredient>): void {
+    ingredientPage.content.forEach(ingredient => {
+      ingredient.data = new Map<string, string>(Object.entries(ingredient.data));
+    });
   }
 }
