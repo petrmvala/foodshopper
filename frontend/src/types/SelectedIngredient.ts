1fe26960963c80d8fb407f8bae00fac6849d98e5
kowodo <stepan.krivanec@gmail.com>
2021-04-28 22:51:05 +0200

    type redefinition

--- frontend/src/types/SelectedIngredient.ts
+++ frontend/src/types/SelectedIngredient.ts
@@ -1,7 +1,18 @@
 import {Ingredient} from './Ingredient';
 
-export interface SelectedIngredient {
+export class SelectedIngredient {
   ingredient: Ingredient;
   amount: number;
+  contributions: Map<string, number>;
+
+  constructor(ingredient: Ingredient) {
+    this.ingredient = ingredient;
+    this.amount = 0;
+    this.contributions = new Map<string, number>();
+    this.ingredient.data.forEach((value, key) => {
+      this.contributions.set(key, 0);
+    });
+
+  }
 }
 
